0d6bd5aeaad449be588619ec238cc30e3918d1ef
xxpantherexx <128511633+xxpantherexx@users.noreply.github.com>
2023-03-21 19:53:50 +0100

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'mysecretkey'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///repartidores.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    GOOGLE_MAPS_API_KEY = 'AIzaSyCZ9yapP5kLGP5zzuIeXaQX2iiT-W0JinA'
+    GOOGLE_MAPS_API_KEY = ''
 
 mimetypes.add_type('text/css', '.css')
 mimetypes.add_type('application/javascript', '.js')
